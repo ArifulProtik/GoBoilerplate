e62ecfeeba8a0d5cc412b518faa985a39ac2ffc8
ArifulProtik <mdarifulislam.protik@gmail.com>
2019-04-23 11:33:15 +0600

    model Fix

--- model/user.go
+++ model/user.go
@@ -1,7 +1,15 @@
-package model 
+package model
 
-type User Struct {
-	// Your User Model
+import "fmt"
 
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
 }
 
+func main() {
+	var p Person
+	fmt.Println(p)
+
+}
